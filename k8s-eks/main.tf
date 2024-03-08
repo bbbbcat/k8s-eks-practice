8b4eadbcca898f414f3b4c79a960932a11f550b9
bbbbcat <bluebluebee86@naver.com>
2024-03-08 16:53:00 +0900

    k8s-eks_2024-03-08

--- k8s-eks/main.tf
+++ k8s-eks/main.tf
@@ -6,6 +6,18 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 locals {
-  region = "us-west-2"
+  region   = "us-west-2"
+  name     = basename(path.cwd)
+  vpc_cidr = "10.1.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  tags = {
+    Cluster = local.name
+  }
 }
