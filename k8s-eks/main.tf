1b67c42bd1ef94c093cff7f68e87441446343c01
bbbbcat <bluebluebee86@naver.com>
2024-03-11 10:52:38 +0900

    k8s-eks-2024-03-11

--- k8s-eks/main.tf
+++ k8s-eks/main.tf
@@ -21,3 +21,45 @@ locals {
     Cluster = local.name
   }
 }
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0"
+
+  cluster_name                   = local.name
+  cluster_version                = "1.28"
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default_node_group = {
+      instance_types = ["t3.small"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+    }
+  }
+  tags = local.tags
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.1"
+
+  name            = local.name
+  cidr            = local.vpc_cidr
+  azs             = local.azs
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 10)]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
